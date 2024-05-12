969f5beae24b967ac67c30a54204f6cd241b5195
Recordum <mingyu.oh15b@gmail.com>
2024-05-12 16:57:21 +0900

    feat: queue offer/poll 구현

---
+++ data-structure/queue/queue.go
@@ -0,0 +1,58 @@
+package queue
+
+import (
+	. "go-lab/data-structure/common"
+	"container/list"
+)
+
+type Queue struct {
+	front *Elem
+	rear  *Elem
+	Len   int
+}
+
+func New() *Queue {
+	list.New()
+	return &Queue{}
+}
+
+func (q *Queue)Front() interface{} {
+	if q.front == nil {
+		return nil
+	}
+	return q.front.Value
+}
+
+func (q *Queue)Rear() interface{} {
+	if q.rear == nil {
+		return nil
+	}
+	return q.rear.Value
+}
+
+//가장 뒤에 요소를 추가한다.
+func (q *Queue) Offer(value interface{}) {
+	//만약 front가 nill 일 경우에는 front와 rear 모두 새로운 요소를 가리키게 한다.
+	if q.front == nil {
+		q.front = &Elem{Value: value}
+		q.rear = q.front
+	} else {
+		newElem := &Elem{Value: value}
+		newElem.Next = q.rear
+		q.rear.Prev = newElem
+		q.rear = newElem
+	}
+	q.Len++
+}
+
+//가장 앞에 있는 요소를 제거하고 반환한다.
+func (q *Queue) Poll() interface{} {
+	if (q.front == nil) {
+		return nil
+	}
+	pollElem := q.front
+	q.front = pollElem.Prev
+	q.front.Next = nil
+	q.Len--
+	return pollElem.Value
+}
---
+++ data-structure/queue/queue_test.go
@@ -0,0 +1,68 @@
+package queue
+
+import "testing"
+
+func TestOffer(t *testing.T) {
+	q := New();
+	q.Offer(1);
+	q.Offer(2);
+	q.Offer(3);
+	
+	if q.Len != 3 {
+		t.Errorf(`Expected 3 but got %d`, q.Len)
+	}
+
+	if q.Front() != 1 {
+		t.Errorf(`Expected 1 but got %d`, q.Front())
+	}
+
+	if q.Rear() != 3 {
+		t.Errorf(`Expected 3 but got %d`, q.Rear())
+	}
+
+}
+
+func TestPoll(t *testing.T) {
+	q := New();
+	q.Offer(1);
+	q.Offer(2);
+	q.Offer(3);
+	
+	if q.Len != 3 {
+		t.Errorf(`Expected 3 but got %d`, q.Len)
+	}
+
+	if q.Poll() != 1 {
+		t.Errorf(`Expected 1 but got %d`, q.Poll())
+	}
+
+	if q.Len != 2 {
+		t.Errorf(`Expected 2 but got %d`, q.Len)
+	}
+
+	if q.Front() != 2 {
+		t.Errorf(`Expected 2 but got %d`, q.Front())
+	}
+
+	if q.Rear() != 3 {
+		t.Errorf(`Expected 3 but got %d`, q.Rear())
+	}
+}
+
+func TestPollEmpty(t *testing.T) {
+	q := New();
+	if q.Poll() != nil {
+		t.Errorf(`Expected nil but got %d`, q.Poll())
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	q := New();
+	if !q.IsEmpty() {
+		t.Errorf(`Expected true but got false`)
+	}
+	q.Offer(1);
+	if q.IsEmpty() {
+		t.Errorf(`Expected false but got true`)
+	}
+}
